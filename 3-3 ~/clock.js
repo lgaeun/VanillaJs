6b0a93396b850d84c546b6afd5e16e573eba86d7
lgaeun <gaeungaeun4837@gmail.com>
2021-01-29 10:03:55 +0900

    Update clock.js

--- 3-3 ~/clock.js
+++ 3-3 ~/clock.js
@@ -1,22 +1,19 @@
 const clockContainer = document.querySelector(".js-clock"),
-      clockTitle = clockContainer.querySelector("h1");
+  clockTitle = clockContainer.querySelector("h1");
 
-function getTime(){
-    const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-        minutes < 10 ? `0${minutes}` : minutes}:${
-        seconds < 10 ? `0${seconds}` : seconds
-    }`; // teneraray operator 사용해서 다 두자리 수로 표시 ex) 0->09로 표시
-    
-
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`; // teneraray operator 사용해서 다 두자리 수로 표시 ex) 0->09로 표시
 }
 
 function init() {
-    getTime();  //get the time
-    setInterval(getTime, 1000); //1초 마다 시간 가져오기
+  getTime(); //get the time
+  setInterval(getTime, 1000); //1초 마다 시간 가져오기
 }
 
 init();
